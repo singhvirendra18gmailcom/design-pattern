40eb070626ee7e41348cc35d479e954c6d0169ed
Virendra Singh <singh.virendra18@gmail.com>
2018-08-01 10:12:06 +0530

    Update Resource.java

--- singleton/Resource.java
+++ singleton/Resource.java
@@ -1,40 +1,81 @@
 package singleton;
 
+/**
+ * The Class Resource.
+ */
 public class Resource {
 
+	/** The id. */
 	private String id;
+	
+	/** The name. */
 	private String name;
 
+	/**
+	 * Instantiates a new resource.
+	 *
+	 * @param id the id
+	 * @param name the name
+	 */
 	private Resource(String id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
 
+	/**
+	 * The Class ResourceGenerator.
+	 */
 	private static class ResourceGenerator {
 		// these values for id or name can be fetched from database or
 		// properties file. or can be hardcoded as per bissiness requirement
+		/** The resource. */
 		private static Resource resource = new Resource("101",
 				"Singleton Resource");
 	}
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 
+	/**
+	 * Gets the.
+	 *
+	 * @return the resource
+	 */
 	public static Resource get() {
 		return ResourceGenerator.resource;
 	}
